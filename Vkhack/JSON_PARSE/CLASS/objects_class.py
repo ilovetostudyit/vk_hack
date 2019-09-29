5cde4641f738ef50040c216a0c76d6607bfc4668
sonya <ilovetostudyit@gmail.com>
2019-09-29 06:19:08 +0300

    push

--- Vkhack/JSON_PARSE/CLASS/objects_class.py
+++ Vkhack/JSON_PARSE/CLASS/objects_class.py
@@ -1,7 +1,15 @@
 import json
+from json_parse.utils import *
+import jellyfish
 class Objects_class(object):
     def __init__(self, objects_dict):
         self.dict = objects_dict
+    def get_object_name(self, object):
+        return(self.dict[object]["name"]["ru"])
+    def get_object_adress(self, object):
+        return(self.dict[object]["adress"]["ru"])
+    def get_object_brief(self, object):
+        return(self.dict[object]["brief"]["ru"])
     def get_list_objects(self):
         test_str = ""
         for object in self.dict:
@@ -9,3 +17,31 @@ class Objects_class(object):
         json_answer = {}
         json_answer["text"] = test_str
         return(json.dumps(json_answer, ensure_ascii=False))
+    def object_info(self, object):
+        text = self.dict[object]["name"]["ru"]
+        if self.dict[object]["year"]:
+            text = text + '\n Год создания:' + str(self.dict[object]["year"])
+        text = text + '\n' + self.dict[object]["text"]["ru"]
+        return(text)
+        
+    def object_search(self, object_name):
+        json_answer = {}
+        json_answer["text"] = ""
+        max = 0
+        tanim_object = 0
+        for object in self.dict:
+            tanim_coof = jellyfish.jaro_distance(self.get_object_name(object).lower(), object_name.lower())
+            if (tanim_coof > max):
+                max = tanim_coof
+                tanim_object = object
+        if (tanim_coof > 0.6):
+            json_answer["text"] = "Я уверена это:"
+        elif (tanim_coof > 0.4):
+            json_answer["text"] = "Мне кажется это:"
+        elif (tanim_coof > 0.2):
+            json_answer["text"] = "Может быть это:"
+        else:    
+            json_answer["text"] = wrong_question()
+            return(json.dumps(json_answer, ensure_ascii=False,))
+        json_answer["text"] = json_answer["text"] + self.object_info(tanim_object)
+        return(json.dumps(json_answer, ensure_ascii=False,))
--- Vkhack/main.py
+++ Vkhack/main.py
@@ -6,15 +6,19 @@ from django.urls import path
 
 def main(argv=None):
     museum = base_navigation()
-    print(museum.buildings.get_museum_status("ГАЛЕРЕЯ", "Понедельник"))
-    print(museum.buildings.get_museum_status("ГитЛЕР", "Вторник"))
-    print(museum.buildings.get_museum_status("рихтера", "Среда"))
-    print(museum.buildings.get_museum_status("депазитарий", "Среда"))
+    #print(museum.buildings.get_museum_status("ГАЛЕРЕЯ", "Понедельник"))
+    #print(museum.buildings.get_museum_status("ГитЛЕР", "Вторник"))
+    #print(museum.buildings.get_museum_status("рихтера", "Среда"))
+    #print(museum.buildings.get_museum_status("депазитарий", "Среда"))
     
-    print(museum.buildings.building_navigation("галерея","расписание"))
-    print(museum.buildings.building_navigation("галерея","контакты"))
-    print(museum.buildings.building_navigation("галерея","яндекс.карты"))
-
+    #print(museum.buildings.building_navigation("галерея","расписание"))
+    #print(museum.buildings.building_navigation("галерея","контакты"))
+    #print(museum.buildings.building_navigation("галерея","яндекс.карты"))
+    #print(museum.objects.get_list_objects())
+    print(museum.objects.object_search("Дева Мария"))
+    print(museum.objects.object_search("Смерть и убийство"))
+    print(museum.objects.object_search("Гай Юлий Цезарь"))
+    print(museum.objects.object_search("Иван Грозный убивает своего сына"))
     return 0
 
 if __name__ == "__main__":
